79ca9ff7ff4894a2c7f3e11ac90972ceff355e26
John Bramley <bramleyjl@gmail.com>
2018-05-07 20:49:47 -0400

    live streams hover events fixed

--- src/components/LiveStreamInfo.js
+++ src/components/LiveStreamInfo.js
@@ -6,13 +6,13 @@ class LiveStreamInfo extends React.Component {
   	const {details} = this.props;
 
     return (
-    	<div className="card liveStream" id={this.props.index}>
-        <div className="card-body">
-  				  <a href={details.channel.url}>
-              <img src={details.preview.medium} className="img-fluid mx-auto rounded" alt={this.props.index} />
-              <h6 className="card-img-overlay live-stream">{details.game}</h6>
-              <h5 className="card-title live-stream">{details.channel.display_name}</h5>
-            </a>
+    	<div className="card" id={this.props.index}>
+			  <a href={details.channel.url}>
+          <img src={details.preview.medium} className="img-fluid mx-auto rounded" alt={this.props.index} />
+          <h6 className="card-img-overlay live-stream">{details.game}</h6>
+          <h5 className="card-title live-stream">{details.channel.display_name}</h5>
+        </a>
+        <div className="card-body d-flex align-items-center justify-content-center">
           <p className="card-text">{details.channel.status}</p>
         </div>
       </div>
--- src/components/OfflineStreamInfo.js
+++ src/components/OfflineStreamInfo.js
@@ -6,11 +6,11 @@ class OfflineStreamInfo extends React.Component {
     const {details} = this.props;
 
     return (
-      <div className="card hoverable" id={this.props.index}>
-          <a href={details.url}>
-            <img src={details.video_banner} className="img-fluid mx-auto rounded" alt={this.props.index} />
-            <h5 className="card-title offline-stream">{details.display_name}</h5>
-          </a>
+      <div className="card" id={this.props.index}>
+        <a href={details.url}>
+          <img src={details.video_banner} className="img-fluid mx-auto rounded" alt={this.props.index} />
+          <h5 className="card-title offline-stream">{details.display_name}</h5>
+        </a>
         <div className="card-body d-flex align-items-center justify-content-center">
           <p className="card-text">{details.status}</p>
         </div>
