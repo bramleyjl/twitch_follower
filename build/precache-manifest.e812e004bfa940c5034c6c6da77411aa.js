a584b94a6732eebe232eddd2084e19724dffe471
John <bramleyjl@gmail.com>
2019-11-09 14:26:01 -0500

    adds build file to commit to avoid having to build on droplet

---
+++ build/precache-manifest.e812e004bfa940c5034c6c6da77411aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bebc0b0e06a88e16c44580a77079df2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7371d2b518f4a576b03d",
+    "url": "./static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "330142294d12822eb0f8",
+    "url": "./static/css/main.07524ffe.chunk.css"
+  },
+  {
+    "revision": "7371d2b518f4a576b03d",
+    "url": "./static/js/2.13aa2c72.chunk.js"
+  },
+  {
+    "revision": "330142294d12822eb0f8",
+    "url": "./static/js/main.3a4eb16a.chunk.js"
+  },
+  {
+    "revision": "29a7987aff190bc4b579",
+    "url": "./static/js/runtime-main.1000e5b8.js"
+  },
+  {
+    "revision": "79cfbf22ae660b08d10556ae204f689f",
+    "url": "./static/media/twitch-background.79cfbf22.jpg"
+  }
+]);
